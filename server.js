374cb909dbed5d2996c88c8bf9530fea9478596f
Tanveer Hossain <stanveer.me@gmail.com>
2025-02-19 10:28:49 +0600

    Add server startup log message and remove old log file

--- server.js
+++ server.js
@@ -154,5 +154,6 @@ app.get("/", (req, res) => {
 
 // Start Server
 app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
   logger.info(`Server running on http://localhost:${PORT}`);
 });
